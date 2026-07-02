8da3fb96c8fd6fec990cb88a13da78ce3b3a9e7e
Tyler Simas <tsimas250@icloud.com>
2026-07-02 14:45:20 -0700

    Added the feedforward

---
+++ src/feedforward.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout = 0.1):
+        self.model = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(d_ff, d_model)
+        )
+    
+    def forward(self, x):
+        return self.model(x)
